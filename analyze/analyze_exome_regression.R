e8bbe1bfcc66baa7c1af704f35d2ef5c9e428aa3
Jake Hughey <jakejhughey@gmail.com>
2018-07-03 14:14:21 -0500

    moved more stuff to functions and moved parameters into yaml file.

--- analyze/analyze_exome_regression.R
+++ analyze/analyze_exome_regression.R
@@ -1,216 +1,110 @@
 source(file.path('analyze', 'analyze_setup.R'))
 
-testing = FALSE
-if (testing) {
-  resultDir = 'results_test'
-}
+cmdArgs = commandArgs(trailingOnly = TRUE)
 
-registerDoParallel(cores = 16)
-filePrefix = 'exome'
+paramDir = 'params'
 
-plinkFilepath = '../plink-1.90b6/plink'
-plinkMemSize = 100 # MB
-plinkVif = 1e7 # reduce plink's tendency to output NA due to multicollinearity of the spline bases
+if (length(cmdArgs) == 0) {
+  paramFile = 'exome_params.yaml'
+} else {
+  paramFile = cmdArgs[1]}
+params = read_yaml(file.path(paramDir, paramFile))
 
-genoDir = '../genotype_data/exome'
-genoPrefix = 'Exome_GRID_Euro'
+registerDoParallel(cores = params$nCores)
 
-minRecLen = 0 # years
-minEvents = 2 # to be defined as a case; controls have zero events
-maxAgeAtEvent = 90 # years; dates in the SD after this age are untrustworthy
-minCases = 50 # to analyze the phecode
 
-nPC = 2 # for both cox and logistic regression
-splineDf = 3 # for last_age in logistic regression (ns::splines)
-buffer = 1 # years; for calculating age1 in cox regression
-
-minMaf = 0.01
-minCallRate = 0.95
-minHwePval = 0.001
+resultDir = paste0(params$filePrefix, format(Sys.time(), '_%Y%m%d_%H%M%S'))
+if (!file.exists(resultDir)) {
+  dir.create(resultDir)}
 
 ############################################################
 # load snp data
 
-snpData = setDT(read_csv(file.path(procDir, 'exome_map.csv.gz'), col_types = cols()))
-snpData = snpData[, .(snp = snp.name, chr = chromosome, pos = position)]
-
-genoData = readRDS(file.path(procDir, sprintf('%s_genotype_data.rds', filePrefix)))
-
-idx = (genoData$genoSummary$MAF >= minMaf) &
-  (genoData$genoSummary$Call.rate >= minCallRate) &
-  (2 * pnorm(-abs(genoData$genoSummary$z.HWE)) >= minHwePval)
-snps = intersect(colnames(genoData$genoFull$genotypes)[idx], snpData[chr <= 22, snp])
-
-if (testing) {
-  snps = unique(read_csv('results/exome_pilot_top20.csv', col_types = cols())$snp)
-  # set.seed(17)
-  # snps = snps[sample.int(length(snps), 1000)]
-}
-
-snpFilepath = tempfile('snp_', fileext = '.tsv')
-write_tsv(data.table(snps), snpFilepath, col_names = FALSE)
+genoKeep = loadGeno(procDir, params$filePrefix, params$geno,
+                    file.path(paramDir, params$snpSubsetFile))
 
 ############################################################
 # load grid data
 
-gridData = read_csv(file.path(procDir, sprintf('%s_grid_data.csv.gz', filePrefix)), col_types = 'ccDDD')
-setDT(gridData)
-gridData[, first_age := time_length(first_entry_date - dob, 'years')]
-gridData[, last_age := time_length(last_entry_date - dob, 'years')]
-gridData[, rec_len := last_age - first_age]
-gridData = gridData[first_age >= 0 & rec_len >= minRecLen]
-
-gridData = cbind(gridData, data.table(splines::ns(gridData$last_age, df = splineDf)))
-colnames(gridData)[(ncol(gridData) - splineDf + 1):ncol(gridData)] = paste0('last_age', 1:splineDf)
-
-covarColnames = c('rec_len', paste0('last_age', 1:splineDf))
-
-# load PC data
-if (nPC > 0) {
-  pcData = setDT(read_csv(file.path(procDir, sprintf('%s_pc_data.csv.gz', filePrefix)), col_types = cols()))
-  gridData = merge(gridData, pcData[, 1:(1 + nPC)], by = 'grid')
-  covarColnames = c(covarColnames, colnames(pcData)[2:(1 + nPC)])}
-
-genoTmp = data.table(grid = genoData$genoFull$fam$pedigree, sex = genoData$genoFull$fam$sex)
-gridData = merge(gridData, genoTmp, by = 'grid')
-
-covarData = gridData[, .(FID = grid, IID = grid)]
-covarData = cbind(covarData, gridData[, c(covarColnames, 'sex'), with = FALSE])
-
-covarFilepath = tempfile('covar_', fileext = '.tsv')
-write_tsv(covarData, covarFilepath)
+gridTmp = loadGrid(procDir, params$filePrefix, params$pheno$minRecLen,
+                   params$gwas$nPC, params$gwas$splineDf, genoKeep$fam)
+gridData = gridTmp[[1]]
+covarColnames = gridTmp[[2]]
+rm(gridTmp)
 
 ############################################################
 # load phenotype data
 
-phenoData = read_csv(file.path(procDir, sprintf('%s_phenotype_data.csv.gz', filePrefix)), col_types = 'ccD')
-setDT(phenoData)
-
-phenoData = merge(phenoData, gridData[, .(grid, dob, sex)], by = 'grid')
-phenoData[, age := time_length(entry_date - dob, 'years')]
-phenoData = phenoData[age <= maxAgeAtEvent]
-
-phenoData = merge(phenoData, phecodeData[, .(phecode, whichSex)], by = 'phecode')
-phenoData = phenoData[(whichSex == 'both') | (whichSex == 'male' & sex == 1) |
-                        (whichSex == 'female' & sex == 2)]
-
-phenoTmp = phenoData[, .N, by = .(grid, phecode)]
-phenoTmp = phenoTmp[, .(nCases = sum(N >= minEvents)), by = phecode]
-phenoData = merge(phenoData, phenoTmp[nCases >= minCases], by = 'phecode')
-phenoData = phenoData[, .(grid, phecode, age)]
-
-if (testing) {
-  phenoData = phenoData[phecode %in% c('274.1', '714.1', '335', '185', '427.21', '290.11')]
-}
+phenoTmp = loadPheno(procDir, params$filePrefix, params$pheno, gridData,
+                     file.path(paramDir, params$phecodeSubsetFile))
+phenoData = phenoTmp[[1]]
+phenoSummary = phenoTmp[[2]]
+rm(phenoTmp)
 
 ############################################################
 
-phecodeDataKeep = phecodeData[phecode %in% unique(phenoData$phecode), .(phecode, whichSex)]
-phecodeDataKeep[, phecodeStr := paste0('phe', gsub('.', 'p', phecode, fixed = TRUE))]
-phecodeDataKeep[, coxFilename := sprintf('%s_%s_cox.tsv.gz', filePrefix, phecodeStr)]
-phecodeDataKeep[, logisticFilename := sprintf('%s_%s_logistic.tsv', filePrefix, phecodeStr)]
-phecodeDataKeep[, covarNum := paste0('1-', ncol(covarData) - ifelse(whichSex == 'both', 2, 3))]
-phecodeDataKeep = merge(phecodeDataKeep, phenoTmp, by = 'phecode')
+gwasMetadata = makeGwasMetadata(params$filePrefix, phecodeData, phenoData, phenoSummary)
 
 # split analysis across servers
-# phecodeDataKeep = phecodeDataKeep[1:round(nrow(phecodeDataKeep) / 2)]
-# phecodeDataKeep = phecodeDataKeep[(1 + round(nrow(phecodeDataKeep) / 2)):nrow(phecodeDataKeep)]
+# gwasMetadata = gwasMetadata[1:round(nrow(gwasMetadata) / 2)]
+# gwasMetadata = gwasMetadata[(1 + round(nrow(gwasMetadata) / 2)):nrow(gwasMetadata)]
 # change names of progress files and workspace file
-
-# phecodeDataKeep = phecodeDataKeep[1:31]
 
 ############################################################
 # run cox regression
 
-logFilepath = file.path(resultDir, sprintf('%s_progress_cox.txt', filePrefix))
-timeStarted = Sys.time()
-cat(sprintf('%s started analysis\n', timeStarted), file = logFilepath)
+coxLog = createLogFile(resultDir, params$filePrefix, 'cox')
 
-phenoLogisticList = foreach(ii = 1:nrow(phecodeDataKeep)) %dopar% {
-  whichSex = phecodeDataKeep$whichSex[ii]
-  phenoDataNow = phenoData[phecode == phecodeDataKeep$phecode[ii], .(grid, age)]
+phenoPlinkList = foreach(ii = 1:nrow(gwasMetadata)) %dopar% {
+  whichSex = gwasMetadata$whichSex[ii]
+  phenoDataNow = phenoData[phecode == gwasMetadata$phecode[ii], .(grid, age)]
 
-  inputBase = makeInput(phenoDataNow, gridData, whichSex, minEvents, buffer)
-  coxStr = makeCoxStr(whichSex, nPC)
+  inputBase = makeInput(phenoDataNow, gridData, whichSex, params$pheno$minEvents,
+                        params$pheno$ageBuffer)
+  coxStr = makeCoxStr(whichSex, params$gwas$nPC)
 
-  resultNow = foreach(snp = snps, .combine = rbind) %do% {
-    inputNow = addSnpToInput(inputBase, genoData$genoFull, snp)
-    coxFit = runCox(coxStr, inputNow)
-    data.table(coef(summary(coxFit))[1, c(1, 3:4), drop = FALSE])}
+  gwasResult = runGwasCox(inputBase, genoKeep, coxStr)
+  write_tsv(gwasResult, gzfile(file.path(resultDir, gwasMetadata$coxFilename[ii])))
 
-  colnames(resultNow) = c('beta', 'se', 'z')
-  resultNow[, pval := 2 * pnorm(-abs(z))] # coxph sets small pvals to zero
-  resultNow[, snp := snps]
-  write_tsv(resultNow, gzfile(file.path(resultDir, phecodeDataKeep$coxFilename[ii])))
+  appendLogFile(coxLog, gwasMetadata, ii)
+  makePhenoPlink(inputBase, gwasMetadata$phecodeStr[ii])}
 
-  cat(sprintf('%s completed phecode %s (%d of %d)\n', Sys.time(), phecodeDataKeep$phecode[ii],
-              ii, nrow(phecodeDataKeep)), file = logFilepath, append = TRUE)
+finishLogFile(coxLog)
 
-  # prepare phenotype data for plink
-  phenoLogisticNow = inputBase[, .(grid, status)]
-  colnames(phenoLogisticNow)[2] = phecodeDataKeep$phecodeStr[ii]
-  setkey(phenoLogisticNow, 'grid')
-  phenoLogisticNow}
+############################################################
+# prepare data for plink
 
-timeElapsed = Sys.time() - timeStarted
-cat(sprintf('Time elapsed of %.2f %s\n', timeElapsed, attr(timeElapsed, 'units')),
-    file = logFilepath, append = TRUE)
+plinkTmp = prepForPlink(colnames(genoKeep$genotypes), gridData, covarColnames,
+                        gwasMetadata, phenoPlinkList)
+gwasMetadata = plinkTmp[[1]]
+plinkPaths = plinkTmp[[2]]
+rm(plinkTmp)
 
 ############################################################
 # run logistic regression in plink
 
-# make phenotype file for plink
-phenoPlink = Reduce(function(...) merge(..., all = TRUE), phenoLogisticList)
-phenoPlink[is.na(phenoPlink)] = -9
+plinkArgs = makePlinkArgs(params$plink, plinkPaths)
+plinkLog = createLogFile(resultDir, params$filePrefix, 'logistic')
 
-colnames(phenoPlink)[1] = 'FID'
-phenoPlink[, IID := FID]
-setcolorder(phenoPlink, c(1, ncol(phenoPlink)))
-
-phenoFilepath = tempfile('pheno_', fileext = '.tsv')
-write_tsv(phenoPlink, phenoFilepath)
-
-plinkArgs = sprintf('%s --bfile %s --extract %s --covar %s --pheno %s --1 --memory %d --vif %d',
-                    '--logistic hide-covar beta --ci 0.95', file.path(genoDir, genoPrefix),
-                    snpFilepath, covarFilepath, phenoFilepath, plinkMemSize, plinkVif)
-
-logFilepath = file.path(resultDir, sprintf('%s_progress_logistic.txt', filePrefix))
-timeStarted = Sys.time()
-cat(sprintf('%s started analysis\n', timeStarted), file = logFilepath)
-
-done = foreach(ii = 1:nrow(phecodeDataKeep)) %dopar% {
+done = foreach(ii = 1:nrow(gwasMetadata)) %dopar% {
   # run plink
-  plinkArgsNow = sprintf('%s --pheno-name %s --covar-number %s --out %s_%s',
-                         plinkArgs, phecodeDataKeep$phecodeStr[ii], phecodeDataKeep$covarNum[ii],
-                         file.path(resultDir, filePrefix), phecodeDataKeep$phecodeStr[ii])
-  system2(plinkFilepath, plinkArgsNow)
+  runGwasPlink(resultDir, params$filePrefix, gwasMetadata$phecodeStr[ii],
+               gwasMetadata$covarNum[ii], plinkArgs, params$plink$execPath)
 
   # fix plink's stupid output spacing
-  outputFilename = sprintf('%s_%s.assoc.logistic', filePrefix, phecodeDataKeep$phecodeStr[ii])
-  outputFilepath = file.path(resultDir, outputFilename)
-  tmpFilepath = tempfile(paste0(phecodeDataKeep$phecodeStr[ii], '_'))
-  system(sprintf("cat %s | tr -s ' ' '\t' > %s", outputFilepath, tmpFilepath))
-  system(sprintf("cat %s | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g' > %s",
-                 tmpFilepath, outputFilepath))
-  unlink(tmpFilepath)
+  outputFilepath = cleanPlinkOutput(resultDir, params$filePrefix, gwasMetadata$phecodeStr[ii])
 
   # rename and compress
-  file.rename(outputFilepath, file.path(resultDir, phecodeDataKeep$logisticFilename[ii]))
-  system2('gzip', paste('-f', file.path(resultDir, phecodeDataKeep$logisticFilename[ii])))
+  file.rename(outputFilepath, file.path(resultDir, gwasMetadata$logisticFilename[ii]))
+  system2('gzip', paste('-f', file.path(resultDir, gwasMetadata$logisticFilename[ii])))
 
-  cat(sprintf('%s completed phecode %s (%d of %d)\n', Sys.time(), phecodeDataKeep$phecode[ii],
-              ii, nrow(phecodeDataKeep)), file = logFilepath, append = TRUE)}
+  appendLogFile(plinkLog, gwasMetadata, ii)}
 
-phecodeDataKeep[, logisticFilename := paste0(logisticFilename, '.gz')]
-# unlink(c(snpFilepath, covarFilepath, phenoFilepath))
-
-timeElapsed = Sys.time() - timeStarted
-cat(sprintf('Time elapsed of %.2f %s\n', timeElapsed, attr(timeElapsed, 'units')),
-    file = logFilepath, append = TRUE)
+gwasMetadata[, logisticFilename := paste0(logisticFilename, '.gz')]
+finishLogFile(plinkLog)
 
 ############################################################
 
-d = c('genoData', 'genoTmp', 'idx', 'pcData', 'phenoTmp', 'phenoLogisticList',
-      'done', 'timeStarted', 'timeElapsed', 'phenoPlink')
-save(list = setdiff(ls(), c(d, 'd')),
-     file = file.path(resultDir, sprintf('%s_workspace.Rdata', filePrefix)))
+d = c('genoAll', 'genoKeep', 'phenoPlinkList', 'done', 'd')
+save(list = setdiff(ls(), d),
+     file = file.path(resultDir, sprintf('%s_workspace.Rdata', params$filePrefix)))
--- analyze/analyze_setup.R
+++ analyze/analyze_setup.R
@@ -6,23 +6,104 @@ library('doParallel')
 library('snpStats')
 library('cowplot')
 library('qqman')
+library('yaml')
 
 procDir = 'processed'
-resultDir = 'results'
+# resultDir = 'results'
 
 phecodeData = setDT(read_csv(file.path(procDir, 'phecode_data.csv.gz'), col_types = 'ccc??????'))
-
-############################################################
 
 eb = element_blank()
 theme_set(theme_light() +
             theme(axis.text = element_text(color = 'black'), strip.text = element_text(color = 'black'),
                   panel.grid.minor = eb, legend.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = 'cm')))
 
+############################################################
+# functions for loading data
+
+loadGeno = function(procDir, filePrefix, p, snpSubsetPath = NULL) {
+  genoData = readRDS(file.path(procDir, sprintf('%s_genotype_data.rds', filePrefix)))
+
+  idx = (genoData$genoSummary$MAF >= p$minMaf) &
+    (genoData$genoSummary$Call.rate >= p$minCallRate) &
+    (2 * pnorm(-abs(genoData$genoSummary$z.HWE)) >= p$minHwePval) &
+    (genoData$genoFull$map$chromosome <= 22)
+
+  genoFull = genoData$genoFull
+  genoFull$genotypes = genoFull$genotypes[, idx]
+  genoFull$map = genoFull$map[idx,]
+
+  if (!is.null(snpSubsetPath) & length(snpSubsetPath) > 0) {
+    snps = unique(read_tsv(snpSubsetPath, col_types = 'c', col_names = FALSE)$X1)
+    genoFull$genotypes = genoFull$genotypes[, colnames(genoFull$genotypes) %in% snps]
+    genoFull$map = genoFull$map[genoFull$map$snp.name %in% snps,]}
+
+  return(genoFull)}
+
+
+loadGrid = function(procDir, filePrefix, minRecLen, splineDf, nPC, fam) {
+  gridData = read_csv(file.path(procDir, sprintf('%s_grid_data.csv.gz', filePrefix)), col_types = 'ccDDD')
+  setDT(gridData)
+  gridData[, first_age := time_length(first_entry_date - dob, 'years')]
+  gridData[, last_age := time_length(last_entry_date - dob, 'years')]
+  gridData[, rec_len := last_age - first_age]
+  gridData = gridData[first_age >= 0 & rec_len >= minRecLen]
+
+  gridData = cbind(gridData, data.table(splines::ns(gridData$last_age, df = splineDf)))
+  colnames(gridData)[(ncol(gridData) - splineDf + 1):ncol(gridData)] = paste0('last_age', 1:splineDf)
+  covarColnames = c('rec_len', paste0('last_age', 1:splineDf))
+
+  if (nPC > 0) {
+    pcData = read_csv(file.path(procDir, sprintf('%s_pc_data.csv.gz', filePrefix)), col_types = cols())
+    setDT(pcData)
+    gridData = merge(gridData, pcData[, 1:(1 + nPC)], by = 'grid')
+    covarColnames = c(covarColnames, colnames(pcData)[2:(1 + nPC)])}
+
+  genoTmp = data.table(fam)[, .(grid = pedigree, sex)]
+  gridData = merge(gridData, genoTmp, by = 'grid')
+
+  return(list(gridData, covarColnames))}
+
+
+loadPheno = function(procDir, filePrefix, p, gridData, phecodeSubsetPath) {
+  phenoData = read_csv(file.path(procDir, sprintf('%s_phenotype_data.csv.gz', filePrefix)),
+                       col_types = 'ccD')
+  setDT(phenoData)
+
+  if (!is.null(phecodeSubsetPath) & length(phecodeSubsetPath) > 0) {
+    phecodes = unique(read_tsv(phecodeSubsetPath, col_types = 'c', col_names = FALSE)$X1)
+    phenoData = phenoData[phecode %in% phecodes]}
+
+  phenoData = merge(phenoData, gridData[, .(grid, dob, sex)], by = 'grid')
+  phenoData[, age := time_length(entry_date - dob, 'years')]
+  phenoData = phenoData[age <= p$maxAgeAtEvent]
+
+  phenoData = merge(phenoData, phecodeData[, .(phecode, whichSex)], by = 'phecode')
+  phenoData = phenoData[(whichSex == 'both') | (whichSex == 'male' & sex == 1) |
+                          (whichSex == 'female' & sex == 2)]
+
+  phenoSummary = phenoData[, .N, by = .(grid, phecode)]
+  phenoSummary = phenoSummary[, .(nCases = sum(N >= p$minEvents)), by = phecode]
+  phenoData = merge(phenoData, phenoSummary[nCases >= p$minCases], by = 'phecode')
+  phenoData = phenoData[, .(grid, phecode, age)]
+  return(list(phenoData, phenoSummary))}
+
+
+############################################################
+# functions for cox regression
+
+makeGwasMetadata = function(filePrefix, phecodeData, phenoData, phenoSummary) {
+  d = phecodeData[phecode %in% unique(phenoData$phecode), .(phecode, whichSex)]
+  d[, phecodeStr := paste0('phe', gsub('.', 'p', phecode, fixed = TRUE))]
+  d[, coxFilename := sprintf('%s_%s_cox.tsv.gz', filePrefix, phecodeStr)]
+  d[, logisticFilename := sprintf('%s_%s_logistic.tsv', filePrefix, phecodeStr)]
+  # d[, covarNum := paste0('1-', ncol(covarData) - ifelse(whichSex == 'both', 2, 3))]
+  return(merge(d, phenoSummary, by = 'phecode'))}
+
 
 # expected colnames in phenoData: grid, age
 # expected colnames in gridData: grid, first_age, last_age
-makeInput = function(phenoData, gridData, whichSex, minEvents, buffer) {
+makeInput = function(phenoData, gridData, whichSex, minEvents, ageBuffer) {
   phenoCase = phenoData
   setkeyv(phenoCase, c('grid', 'age'))
   phenoCase = phenoCase[, if (.N >= minEvents) .SD[minEvents,], by = grid]
@@ -38,7 +119,7 @@ makeInput = function(phenoData, gridData, whichSex, minEvents, buffer) {
 
   input[, status := ifelse(is.na(age), 0, 1)]
   input[, age2 := ifelse(status, age, last_age)]
-  input[, age1 := min(first_age, max(0, age2 - buffer)), by = grid]
+  input[, age1 := min(first_age, max(0, age2 - ageBuffer)), by = grid]
   input}
 
 
@@ -58,6 +139,94 @@ makeCoxStr = function(whichSex, nPC) {
 
 runCox = function(formulaStr, input) {
   coxph(formula(formulaStr), data = input)}
+
+
+
+runGwasCox = function(inputBase, genoFull, formulaStr) {
+  snps = colnames(genoFull$genotypes)
+  d = foreach(snp = snps, .combine = rbind) %do% {
+    inputNow = addSnpToInput(inputBase, genoFull, snp)
+    coxFit = runCox(formulaStr, inputNow)
+    data.table(coef(summary(coxFit))[1, c(1, 3:4), drop = FALSE])}
+  colnames(d) = c('beta', 'se', 'z')
+  d[, pval := 2 * pnorm(-abs(z))] # coxph sets small pvals to zero
+  d[, snp := snps]
+  return(d)}
+
+############################################################
+# functions for log files
+
+createLogFile = function(resultDir, filePrefix, fileSuffix) {
+  path = file.path(resultDir, sprintf('%s_progress_%s.txt', filePrefix, fileSuffix))
+  timeStarted = Sys.time()
+  cat(sprintf('%s started analysis\n', timeStarted), file = path)
+  return(list(path = path, timeStarted = timeStarted))}
+
+
+appendLogFile = function(logFile, gwasMetadata, ii) {
+  cat(sprintf('%s completed phecode %s (%d of %d)\n', Sys.time(), gwasMetadata$phecode[ii],
+              ii, nrow(gwasMetadata)), file = logFile$path, append = TRUE)}
+
+
+finishLogFile = function(logFile) {
+  timeElapsed = Sys.time() - logFile$timeStarted
+  cat(sprintf('Time elapsed of %.2f %s\n', timeElapsed, attr(timeElapsed, 'units')),
+      file = logFile$path, append = TRUE)}
+
+############################################################
+# functions for logistic regression using plink
+
+makePhenoPlink = function(inputBase, phecodeStr) {
+  phenoPlinkNow = inputBase[, .(grid, status)]
+  colnames(phenoPlinkNow)[2] = phecodeStr
+  setkey(phenoPlinkNow, 'grid')
+  return(phenoPlinkNow)}
+
+
+prepForPlink = function(snps, gridData, covarColnames, gwasMetadata, phenoPlinkList) {
+  snpPath = tempfile('snp_', fileext = '.tsv')
+  write_tsv(data.table(snps), snpPath, col_names = FALSE)
+
+  covarData = gridData[, .(FID = grid, IID = grid)]
+  covarData = cbind(covarData, gridData[, c(covarColnames, 'sex'), with = FALSE])
+  gwasMetadata[, covarNum := paste0('1-', ncol(covarData) - ifelse(whichSex == 'both', 2, 3))]
+  covarPath = tempfile('covar_', fileext = '.tsv')
+  write_tsv(covarData, covarPath)
+
+  phenoPlink = Reduce(function(...) merge(..., all = TRUE), phenoPlinkList)
+  phenoPlink[is.na(phenoPlink)] = -9
+  colnames(phenoPlink)[1] = 'FID'
+  phenoPlink[, IID := FID]
+  setcolorder(phenoPlink, c(1, ncol(phenoPlink)))
+  phenoPath = tempfile('pheno_', fileext = '.tsv')
+  write_tsv(phenoPlink, phenoPath)
+
+  return(list(gwasMetadata, list(snp = snpPath, covar = covarPath, pheno = phenoPath)))}
+
+
+makePlinkArgs = function(p, paths) {
+  sprintf('%s --bfile %s --extract %s --covar %s --pheno %s --1 --memory %d --vif %d',
+          '--logistic hide-covar beta --ci 0.95',
+          file.path(p$dataPath, p$dataPrefix),paths$snp, paths$covar,
+          paths$pheno, as.numeric(p$memSize), as.numeric(p$maxVif))}
+
+
+runGwasPlink = function(resultDir, filePrefix, phecodeStr, covarNum, plinkArgs, execPath) {
+  argsNow = sprintf('%s --pheno-name %s --covar-number %s --out %s_%s',
+                    plinkArgs, phecodeStr, covarNum,
+                    file.path(resultDir, filePrefix), phecodeStr)
+  system2(execPath, argsNow)}
+
+
+cleanPlinkOutput = function(resultDir, filePrefix, phecodeStr) {
+  outputFilename = sprintf('%s_%s.assoc.logistic', filePrefix, phecodeStr)
+  outputFilepath = file.path(resultDir, outputFilename)
+  tmpFilepath = tempfile(paste0(phecodeStr, '_'))
+  system(sprintf("cat %s | tr -s ' ' '\t' > %s", outputFilepath, tmpFilepath))
+  system(sprintf("cat %s | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g' > %s",
+                 tmpFilepath, outputFilepath))
+  unlink(tmpFilepath)
+  return(outputFilepath)}
 
 
 # makeGlmStr = function(whichSex, nPC, splineDf) {
