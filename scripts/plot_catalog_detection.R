cb6b405292320706a129a2682bd8811ba6168092
Jake Hughey <jakejhughey@gmail.com>
2019-01-29 11:55:08 -0600

    added curve for consecutive strategy.

--- scripts/plot_catalog_detection.R
+++ scripts/plot_catalog_detection.R
@@ -41,16 +41,18 @@ setDT(catalogAssocData)
 
 nAssoc = nrow(unique(catalogAssocData[, .(phecode, ldBlock)]))
 
-a = merge(gwasData[, .(phecode, snp, method, pval)],
-          catalogAssocData[, .(phecode, snp, ldBlock)],
-          by = c('phecode', 'snp'))
+a = unique(merge(gwasData[, .(phecode, snp, method, pval)],
+                 catalogAssocData[, .(phecode, snp, ldBlock)],
+                 by = c('phecode', 'snp')))
 
 ############################################################
-############################################################
-# how to deal with snps that are NA in logistic: don't convert them to 1
-# pvalConsecutive := ifelse(pvalLogistic <= 1e-4, pvalCox, pvalLogistic)
 
-############################################################
+pvalConsecCutoff = 1e-4 # must be <= maxPvalLoad
+aCast = dcast(a, phecode + ldBlock + snp ~ method, value.var = 'pval')
+aCast[, consecutive := ifelse(logistic <= pvalConsecCutoff, cox, logistic)]
+a = melt(aCast, measure.vars = c('logistic', 'cox', 'consecutive'),
+         variable.name = 'method', value.name = 'pval')
+
 ############################################################
 
 negLogPvalCutoffs = seq(5, 30, 0.02)
@@ -59,20 +61,23 @@ a1 = foreach(negLogPvalNow = negLogPvalCutoffs, .combine = rbind) %dopar% {
   aNow = a[pval <= 10^(-negLogPvalNow),
            .(nDetected = uniqueN(.SD, by = c('phecode', 'ldBlock'))),
            by = method]
-  aNow[, negLogPvalCutoff := negLogPvalNow]
-  aNow}
+  aNow[, negLogPvalCutoff := negLogPvalNow][]}
 
-a1[, method := ifelse(method == 'cox', 'Cox', method)]
+a1$method = factor(a1$method, levels(a1$method), c('logistic', 'Cox', 'consecutive'))
 a1[, fracDetected := nDetected / nAssoc]
 
 p1 = ggplot(a1) +
-  geom_line(aes(x = negLogPvalCutoff, y = fracDetected, color = method),
-            size = 0.75) +
+  geom_line(aes(x = negLogPvalCutoff, y = fracDetected, color = method,
+                linetype = method), size = 0.8) +
   labs(x = expression(-log[10](p)~cutoff),
-       y = 'Sensitivity for\nknown associations', color = 'Method') +
+       y = 'Sensitivity for\nknown associations', color = 'Method',
+       linetype = 'Method') +
   scale_x_continuous(limits = c(5, 9)) +
   scale_y_continuous(limits = c(0.02, 0.055)) +
-  scale_color_brewer(type = 'qual', palette = 'Set2')
+  scale_color_manual(values = c('#33a02c', '#1f78b4', '#a6cee3')) +
+  scale_linetype_manual(values = c('solid', 'solid', 'dashed')) +
+  guides(color = guide_legend(keywidth = 2, keyheight = 1),
+         linetype = guide_legend(keywidth = 2, keyheight = 1))
 
 
 a2 = a1[, .(relDiffDetected = (nDetected[method == 'Cox'] -
@@ -82,7 +87,7 @@ a2 = a1[, .(relDiffDetected = (nDetected[method == 'Cox'] -
 
 p2 = ggplot(a2) +
   geom_line(aes(x = negLogPvalCutoff, y = relDiffDetected, color = 'raw'),
-            size = 0.75) +
+            size = 0.8) +
   geom_smooth(aes(x = negLogPvalCutoff, y = relDiffDetected, color = 'smoothed'),
               span = 0.5, se = FALSE, size = 0.75,
               show.legend = TRUE) +
@@ -90,8 +95,9 @@ p2 = ggplot(a2) +
        y = 'Rel. change in sensitivity\nfrom logistic to Cox') +
   scale_x_continuous(limits = c(5, 9)) +
   scale_y_continuous(limits = c(0, 0.2)) +
-  scale_color_manual(name = 'Type', values = c('darkgray', 'black'))
+  scale_color_manual(name = 'Type', values = c('darkgray', 'black')) +
+  guides(color = guide_legend(keywidth = 2, keyheight = 1))
 
 p = plot_grid(p1, p2, ncol = 1, labels = 'AUTO', align = 'v', axis = 'lr')
 ggsave(file.path(plotDir, 'summary_catalog_sensitivity.pdf'),
-       plot = p, width = 4.25, height = 5.1)
+       plot = p, width = 4.5, height = 5.1)
